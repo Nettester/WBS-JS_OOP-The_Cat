047372402ba4189e4c6352abb7cdce5ad8b069d2
Nettester <github@gluch.eu>
2022-06-16 11:00:48 +0200

    add cat

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 class Cat {
-    
+    tiredness;
+    hunger;
+    loneliness;
+    happiness;
+
+    feed() {
+        this.hunger = "not hungry";
+        this.tiredness = "tired"
+    }
+
+    sleep(){
+        this.tiredness = "well rested"
+    }
+
+    pet(){
+
+    }
+
 }
